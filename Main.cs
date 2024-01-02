37ce0870292254796d96e35890e6f852928badc8
Mae <mae@weatherelectric.xyz>
2024-01-01 23:30:34 -0800

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,54 @@
+﻿using MarrowUtils.Utilities.LevelReloader;
+using MarrowUtils.Utilities.TheJanitor;
+using MarrowUtils.Utilities.WarehouseScanner;
+
+namespace MarrowUtils;
+
+public class Main : MelonMod
+{
+    internal const string Name = "MarrowUtils";
+    internal const string Description = "Template Mod";
+    internal const string Author = "SoulWithMae";
+    internal const string Company = "Weather Electric";
+    internal const string Version = "0.0.1";
+    internal const string DownloadLink = null;
+
+    internal static MenuCategory MenuCat { get; private set; }
+    internal static Assembly CurrAsm { get; } = Assembly.GetExecutingAssembly();
+
+    public override void OnInitializeMelon()
+    {
+        ModConsole.Setup(LoggerInstance);
+        Preferences.Setup();
+        RegisterUtilities();
+        var mainCat = MenuManager.CreateCategory("Weather Electric", "#6FBDFF");
+        MenuCat = mainCat.CreateCategory("Marrow Utils", Color.gray);
+        Utility.StartAll();
+        Utility.CreateMenus();
+        Hooking.OnLevelInitialized += OnLevelLoad;
+        Hooking.OnLevelUnloaded += OnLevelUnload;
+    }
+
+    private static void RegisterUtilities()
+    {
+        ModConsole.Msg("Registering utilities...", 1);
+        _ = new LevelReloader();
+        _ = new Janitor();
+        _ = new WarehouseScanner();
+    }
+
+    public override void OnUpdate()
+    {
+        Utility.OnUpdate();
+    }
+
+    private static void OnLevelLoad(LevelInfo levelInfo)
+    {
+        Utility.LevelLoad(levelInfo);
+    }
+
+    private static void OnLevelUnload()
+    {
+        Utility.LevelUnload();
+    }
+}
---
+++ Utilities/WarehouseScanner/Monobehaviours/Scanner.cs
@@ -0,0 +1,123 @@
+﻿namespace MarrowUtils.Utilities.WarehouseScanner.Monobehaviours;
+
+[RegisterTypeInIl2Cpp]
+public class Scanner : MonoBehaviour
+{
+    public static Scanner Instance { get; private set; }
+
+    internal static void PostDestroy()
+    {
+        Instance = null;
+    }
+    
+    private Transform _firePoint;
+    private AudioSource _successSound;
+    private ImpactSFX _impactSfx;
+    private TextMeshPro _titleText;
+    private TextMeshPro _barcodeText;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    private void Start()
+    {
+        // PLEASE CAN WE JUST MOVE ON TO ML 0.6 FOR BUILT-IN FIELDINJECTION
+        // ALTERNATIVELY, CAM PLEASE GIVE ME THE MONO BUILD
+        _firePoint = transform.Find("FirePoint");
+        _titleText = transform.Find("Text/Title").GetComponent<TextMeshPro>();
+        _barcodeText = transform.Find("Text/Barcode").GetComponent<TextMeshPro>();
+        _successSound = transform.GetComponent<AudioSource>();
+        // since this is a code mod I can put the game's mixers YEAA FUCK YOU SDK MODDERS I GET TO DO THIS!
+        _successSound.outputAudioMixerGroup = Audio.SFXMixer;
+        _impactSfx = transform.Find("ImpactSFX").GetComponent<ImpactSFX>();
+        _impactSfx.outputMixer = Audio.SFXMixer;
+    }
+
+    public void Scan()
+    {
+        Physics.Raycast(_firePoint.position, _firePoint.forward, out var hit, 100f);
+        // can raycasts just drown already?
+        if (hit.rigidbody == null) return;
+        var poolee = hit.rigidbody.gameObject.GetComponent<AssetPoolee>();
+        // there used to be a dumb check here, it's dumb, I killed it, he wasn't a good employee anyways
+        if (poolee == null)
+        {
+            // Do an additional check as it may be an NPC where the poolee isn't on a rigidbody.
+            ScanNPC(hit);
+            return;
+        }
+        _successSound.Play();
+        var barcode = poolee.spawnableCrate.Barcode;
+        var title = poolee.spawnableCrate.Title;
+        _titleText.text = title;
+        _barcodeText.text = barcode;
+        // i mean I can use a coroutine, buut i dont wanna make jevilib a dependency
+        // hey look at me actually using comments, i never do this
+        // cancel any previous invokes to prevent the text from disappearing too early
+        CancelInvoke(nameof(HideText));
+        Invoke(nameof(HideText), 2f);
+        ModConsole.Msg($"{poolee.gameObject.name}'s barcode: {barcode}, title: {title}");
+    }
+
+    public void ScanMap()
+    {
+        var map = SceneStreamer.Session.Level;
+        if (map == null) return;
+        _successSound.Play();
+        var barcode = map.Barcode;
+        var title = map.Title;
+        _titleText.text = title;
+        _barcodeText.text = barcode;
+        CancelInvoke(nameof(HideText));
+        Invoke(nameof(HideText), 2f);
+        ModConsole.Msg($"Map barcode: {barcode}, title: {title}");
+    }
+
+    private void ScanNPC(RaycastHit hit)
+    {
+        var poolee = hit.rigidbody.gameObject.GetComponentInParent<AssetPoolee>();
+        if (poolee == null)
+        {
+            ScanAvatar(hit);
+            return;
+        }
+        var barcode = poolee.spawnableCrate.Barcode;
+        var title = poolee.spawnableCrate.Title;
+        _titleText.text = title;
+        _barcodeText.text = barcode;
+        CancelInvoke(nameof(HideText));
+        Invoke(nameof(HideText), 2f);
+        ModConsole.Msg($"{poolee.gameObject.name}'s barcode: {barcode}, title: {title}");
+    }
+
+    private void ScanAvatar(RaycastHit hit)
+    {
+        var rm = hit.rigidbody.gameObject.GetComponentInParent<RigManager>();
+        if (rm == null) return;
+        _successSound.Play();
+        var barcode = rm.AvatarCrate.Crate.Barcode;
+        var title = rm.AvatarCrate.Crate.Title;
+        _titleText.text = title;
+        _barcodeText.text = barcode;
+        CancelInvoke(nameof(HideText));
+        Invoke(nameof(HideText), 2f);
+        ModConsole.Msg($"Avatar's barcode: {barcode}, title: {title}");
+    }
+
+    private void HideText()
+    {
+        // NO! GO AWAY TEXT! FUCK YOU!
+        _titleText.text = "";
+        _barcodeText.text = "";
+    }
+    
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+    
+    // needed to register in il2cpp, otherwise useless
+    public Scanner(IntPtr ptr) : base(ptr) {}
+}
